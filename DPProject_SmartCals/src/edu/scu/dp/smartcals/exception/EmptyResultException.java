481239727103f4f15ee3ef4295b4a8f3b0f9358d
aparna g <aparna.ganesh1542@gmail.com>
2015-08-06 22:31:45 -0700

    Changed ProductNotFoundException to EmptyResultException.

---
+++ DPProject_SmartCals/src/edu/scu/dp/smartcals/exception/EmptyResultException.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package edu.scu.dp.smartcals.exception;
+
+/**
+ * @author Aparna Ganesh
+ *
+ */
+public class EmptyResultException extends Exception {
+
+	public EmptyResultException() {
+		super("Result Not found");
+	}
+}
