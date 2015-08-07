fa49a0c794acb13797bce6e8862e885e9dfc2f8d
aparna g <aparna.ganesh1542@gmail.com>
2015-08-06 22:33:03 -0700

    Generated Auto Getters Setters.

--- DPProject_SmartCals/src/abstractfactory/Product.java
+++ DPProject_SmartCals/src/abstractfactory/Product.java
@@ -14,7 +14,39 @@ public abstract class Product {
 	private String productPrice;
 	private long itemCode;
 	
-	public abstract void getCategory();
+	
+	public String getProdCategory() {
+		return prodCategory;
+	}
+	public void setProdCategory(String prodCategory) {
+		this.prodCategory = prodCategory;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public long getProductID() {
+		return productID;
+	}
+	public void setProductID(long productID) {
+		this.productID = productID;
+	}
+	public String getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(String productPrice) {
+		this.productPrice = productPrice;
+	}
+	public long getItemCode() {
+		return itemCode;
+	}
+	public void setItemCode(long itemCode) {
+		this.itemCode = itemCode;
+	}
+	
+	/*public abstract void getCategory();
 	public abstract void setCategory();
 	
 	public abstract void getName();
@@ -30,6 +62,6 @@ public abstract class Product {
 	public abstract void setImage();
 	
 	public abstract void getNutriInfo(long prodID);
-	public abstract void setNutriInfo(long prodID);
+	public abstract void setNutriInfo(long prodID);*/
 
 }
