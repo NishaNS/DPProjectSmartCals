b876a016a992acaa318b46d9b6dbae08823dda45
aparna g <aparna.ganesh1542@gmail.com>
2015-08-13 11:23:21 -0700

    No specific implementation here.

--- DPProject_SmartCals/src/edu/scu/dp/smartcals/vm/HospitalVendingMachineFactory.java
+++ DPProject_SmartCals/src/edu/scu/dp/smartcals/vm/HospitalVendingMachineFactory.java
@@ -3,12 +3,13 @@
  */
 package edu.scu.dp.smartcals.vm;
 
+import edu.scu.dp.smartcals.model.ProductModel;
 import edu.scu.dp.smartcals.model.VendingMachineModel;
 
 /**
  * Factory class to return Hospital Vending Machine
  * @author Aparna Ganesh
- *
+ *No Implementation provided so far-TODO
  */
 public class HospitalVendingMachineFactory extends VendingMachineFactory {
 
@@ -22,6 +23,33 @@ public class HospitalVendingMachineFactory extends VendingMachineFactory {
 		//TODO set id,location, status for hospital implementation
 		return null;
 	}
+
+/* (non-Javadoc)
+ * @see edu.scu.dp.smartcals.vm.VendingMachineFactory#createBreverage(edu.scu.dp.smartcals.model.ProductModel)
+ */
+@Override
+public Beverage createBreverage(ProductModel productModel) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+/* (non-Javadoc)
+ * @see edu.scu.dp.smartcals.vm.VendingMachineFactory#createCandy(edu.scu.dp.smartcals.model.ProductModel)
+ */
+@Override
+public Candy createCandy(ProductModel productModel) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+/* (non-Javadoc)
+ * @see edu.scu.dp.smartcals.vm.VendingMachineFactory#createSnack(edu.scu.dp.smartcals.model.ProductModel)
+ */
+@Override
+public Snack createSnack(ProductModel productModel) {
+	// TODO Auto-generated method stub
+	return null;
+}
 	}
 
 
