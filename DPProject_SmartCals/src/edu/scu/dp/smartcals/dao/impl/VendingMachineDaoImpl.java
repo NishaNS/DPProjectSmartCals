3a1c24484770c62047d0e2d2273d26b0cde334df
aparna g <aparna.ganesh1542@gmail.com>
2015-08-13 11:52:58 -0700

    Added comments

--- DPProject_SmartCals/src/edu/scu/dp/smartcals/dao/impl/VendingMachineDaoImpl.java
+++ DPProject_SmartCals/src/edu/scu/dp/smartcals/dao/impl/VendingMachineDaoImpl.java
@@ -20,41 +20,42 @@ import edu.scu.dp.smartcals.model.VendingMachineModel;
 
 /**
  * Class to perform operations from Vending machine table
+ * 
  * @author Aparna Ganesh
  *
  */
 public class VendingMachineDaoImpl implements VendingMachineDao {
 
-	
-
 	DatabaseFactory databaseFactory;
-	
+
 	private static VendingMachineDao INSTANCE;
-	
 
 	private VendingMachineDaoImpl(DatabaseFactory databaseFactory) {
 
 		this.databaseFactory = databaseFactory;
 	}
+
 	/**
-	 * Implementation of Singleton pattern as there should be only VendingMachineDAO for 
-	 * the entire application
+	 * Implementation of Singleton pattern as there should be only
+	 * VendingMachineDAO for the entire application
+	 * 
 	 * @param databaseFactory
 	 * @return
 	 */
 	public static VendingMachineDao getInstance(DatabaseFactory databaseFactory) {
-		if(INSTANCE == null) {
+		if (INSTANCE == null) {
 			INSTANCE = new VendingMachineDaoImpl(databaseFactory);
 		}
 		return INSTANCE;
 	}
-	 
+
 	/**
-	 * Returns all the vending machines basic info.
+	 * Returns all the vending machines basic info.-Aparna
 	 */
 
 	@Override
-	public List<VendingMachineModel> getAllVMBasicInfo() throws EmptyResultException, SQLException {
+	public List<VendingMachineModel> getAllVMBasicInfo()
+			throws EmptyResultException, SQLException {
 		PreparedStatement statement = null;
 		Connection connection = databaseFactory.getConnection();
 		List<VendingMachineModel> vendingMachines = new ArrayList<>();
@@ -65,11 +66,9 @@ public class VendingMachineDaoImpl implements VendingMachineDao {
 
 			ResultSet rs = statement.executeQuery();
 			while (rs.next()) {
-				vendingMachines.add(mapRow(rs));
+				vendingMachines.add(mapVmBasicRow(rs));
 
-			} 
-				
-			
+			}
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -81,20 +80,79 @@ public class VendingMachineDaoImpl implements VendingMachineDao {
 		return vendingMachines;
 
 	}
-	
-		
+
+	/**
+	 * Returns a VM with all products for a given Id-Aparna
+	 */
+	@Override
+	public VendingMachineModel getVendingMachine(long vmId) throws EmptyResultException, SQLException {
+
+		final String VM_SQL = "select * from vendingmachine where VendingMachineID = ?";
+		Connection connection = databaseFactory.getConnection();
+		PreparedStatement statement = connection.prepareStatement(VM_SQL);
+		statement.setLong(1, vmId);
+		ResultSet rs = statement.executeQuery();
+		VendingMachineModel vmModel = null;
+		if (rs.next()) {
+			vmModel = mapVmBasicRow(rs);
+		} else {
+			throw new EmptyResultException(
+					"No result found for Vending machine :" + vmId);
+		}
+
+	//Gets all products for a given VM Id
+		List<ProductModel> productModels = getProductsByVMId(vmId);
+		vmModel.setProductModels(productModels);
+
+		return vmModel;
+
+	}
+
+	/**
+	 * Returns all products for a selected VM id-Aparna
+	 * 
+	 * @throws SQLException
+	 */
+	@Override
+	public List<ProductModel> getProductsByVMId(long vmId) throws SQLException {
+		PreparedStatement statement = null;
+		Connection connection = databaseFactory.getConnection();
+		List<ProductModel> products = new ArrayList<>();
+
+		try {
+			statement = connection
+					.prepareStatement("select prod.ProductID,inven.Price,prod.ProductName,prod.Category from product prod, inventory inven  where inven.VendingMachineID=? and prod.ProductID = inven.ProductID");
+			statement.setLong(1, vmId);
+			ResultSet rs = statement.executeQuery();
+			while (rs.next()) {
+				products.add(ProductRowMapper.mapRow(rs));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			DBUtils.closeStatement(statement);
+			databaseFactory.closeConnection();
+		}
+		return products;
+	}
+
 	/**
 	 * VendingMachine Row Mapper
+	 * 
 	 * @param resultSet
 	 * @return
 	 * @throws SQLException
 	 */
-	private VendingMachineModel mapRow(ResultSet rs) throws SQLException {
+	private VendingMachineModel mapVmBasicRow(ResultSet rs) throws SQLException {
 		VendingMachineModel vendingMachine = new VendingMachineModel();
 		vendingMachine.setVendingMachineId(rs.getLong("VendingMachineID"));
 		vendingMachine.setLocation(rs.getString("Location"));
-		vendingMachine.setType(VMLocationType.valueOf(rs.getString("type").toUpperCase()));
-		vendingMachine.setStatus(VMStatus.valueOf(rs.getString("Status").toUpperCase()));
+		vendingMachine.setType(VMLocationType.valueOf(rs.getString("type")
+				.toUpperCase()));
+		vendingMachine.setStatus(VMStatus.valueOf(rs.getString("Status")
+				.toUpperCase()));
 		return vendingMachine;
 	}
 
