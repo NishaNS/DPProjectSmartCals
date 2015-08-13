3ce48e1a2316dbb21a1ef9bed7f8ec1cfd64dc91
aparna g <aparna.ganesh1542@gmail.com>
2015-08-13 11:26:49 -0700

    Added comment

--- DPProject_SmartCals/src/edu/scu/dp/smartcals/vm/Snack.java
+++ DPProject_SmartCals/src/edu/scu/dp/smartcals/vm/Snack.java
@@ -1,6 +1,10 @@
 package edu.scu.dp.smartcals.vm;
 
-
-public abstract class Snack extends Product {
+/**
+ * Snack class to add more properties to Snack-for extensibility purpose
+ * @author Aparna Ganesh
+ *
+ */
+public class Snack extends Product {
 
 }
