02e877b982b14aeedaec045e1f8ea393e8dcd4c5
aparna g <aparna.ganesh1542@gmail.com>
2015-08-13 11:55:02 -0700

    Added comments

--- DPProject_SmartCals/src/edu/scu/dp/smartcals/vm/VMController.java
+++ DPProject_SmartCals/src/edu/scu/dp/smartcals/vm/VMController.java
@@ -5,37 +5,50 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import edu.scu.dp.smartcals.constants.ProductCategory;
 import edu.scu.dp.smartcals.constants.VMLocationType;
 import edu.scu.dp.smartcals.dao.impl.DaoFactory;
+import edu.scu.dp.smartcals.dao.interfaces.ProductDao;
 import edu.scu.dp.smartcals.dao.interfaces.VendingMachineDao;
 import edu.scu.dp.smartcals.exception.DatabaseInitializationException;
 import edu.scu.dp.smartcals.exception.EmptyResultException;
+import edu.scu.dp.smartcals.model.ProductModel;
 import edu.scu.dp.smartcals.model.VendingMachineModel;
 
 /**
  * @author Aparna Ganesh
- * @author Nisha Narayanaswamy
- * VMController class decides the views to be displayed on user action, delegates the call to required classes
+ * @author Nisha Narayanaswamy VMController class decides the views to be
+ *         displayed on user action, delegates the call to required classes
  *
  */
 public class VMController {
-	
+
 	private VendingMachineDao vendingMachineDao;
+
+	private ProductDao productDao;
+
 	
+
 	public VMController() {
 		try {
-			//TODO This factory intialize part shud be done only once for the entire application. Do it where appl starts
-			//for testing purpose its here
+			// TODO This factory intialize part shud be done only once for the
+			// entire application. Do it where appl starts
+			// for testing purpose its here
 			DaoFactory.initialize();
 		} catch (DatabaseInitializationException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
+		//initializing the daos' used here
 		vendingMachineDao = DaoFactory.getVendingMachineDao();
+		productDao = DaoFactory.getProductDao();
 	}
+
 	/**
-	 * Returns all the Vending Machines from Database to ViewAllVendingMachines user interface.
-	 * Converting Vending Machine Model objects to Vending Machine objects
+	 * Returns all the Vending Machines from Database to ViewAllVendingMachines
+	 * user interface. Converting Vending Machine Model objects to Vending
+	 * Machine objects
+	 * 
 	 * @return
 	 */
 	public List<VendingMachine> getAllVendingMachines() {
@@ -48,14 +61,76 @@ public class VMController {
 			e.printStackTrace();
 			return Collections.emptyList();
 		}
-		
-		for(VendingMachineModel vmModel : vendingMachineModels ) {
-			
-			VendingMachineFactory vendingMachineFactory = VendingMachineFactory.getFactory(vmModel.getType());
-			VendingMachine vendingMachine = vendingMachineFactory.createVendingMachine(vmModel);
+
+		for (VendingMachineModel vmModel : vendingMachineModels) {
+
+			VendingMachineFactory vendingMachineFactory = VendingMachineFactory
+					.getFactory(vmModel.getType());
+			VendingMachine vendingMachine = vendingMachineFactory
+					.createVendingMachine(vmModel);
 			vendingMachines.add(vendingMachine);
 		}
 		return vendingMachines;
+	}
+
+	/**
+	 * Returns all the products for a given VM
+	 * 
+	 * @param vmId
+	 * @return
+	 */
+	public VendingMachine getVendingMachine(long vmId) {
+
+		assert (vmId != 0);
+
+		VendingMachineModel vmModel;
+
+		List<Beverage> beverages = new ArrayList<>();
+
+		List<Candy> candies = new ArrayList<>();
+
+		List<Snack> snacks = new ArrayList<>();
+
+		try {
+			vmModel = vendingMachineDao.getVendingMachine(vmId);
+		} catch (SQLException | EmptyResultException e) {
+			
+			e.printStackTrace();
+			return null;
+
+		}
+		
+		VendingMachineFactory vendingMachineFactory = VendingMachineFactory.getFactory(vmModel.getType());
+
+		VendingMachine vendingMachine = vendingMachineFactory.createVendingMachine(vmModel);
+
+		List<ProductModel> productModels = vmModel.getProductModels();
+
+		for (ProductModel productModel : productModels) {
+			
+			switch (productModel.getCategory()) {
+			case BEVERAGE:
+				Beverage breverage = vendingMachineFactory
+				.createBreverage(productModel);
+				beverages.add(breverage);
+				break;
+			case CANDY:
+				Candy candy = vendingMachineFactory.createCandy(productModel);
+				candies.add(candy);
+				break;
+			case SNACK:
+				Snack snack = vendingMachineFactory.createSnack(productModel);
+				snacks.add(snack);
+				break;
+
+			}
+		}
+		vendingMachine.setBeverages(beverages);
+		vendingMachine.setCandies(candies);
+		vendingMachine.setSnacks(snacks);
+
+		return vendingMachine;
+
 	}
 
 }
