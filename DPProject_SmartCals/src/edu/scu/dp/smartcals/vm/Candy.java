25cc478dabcd0d711debdb6d635b7282923206f4
aparna g <aparna.ganesh1542@gmail.com>
2015-08-13 11:22:39 -0700

    Added comment.

--- DPProject_SmartCals/src/edu/scu/dp/smartcals/vm/Candy.java
+++ DPProject_SmartCals/src/edu/scu/dp/smartcals/vm/Candy.java
@@ -1,6 +1,10 @@
 package edu.scu.dp.smartcals.vm;
 
-
-public abstract class Candy extends Product {
+/**
+ * Candy class to add more properties to Candy-for extensibility purpose
+ * @author Aparna Ganesh
+ *
+ */
+public class Candy extends Product {
 
 }
