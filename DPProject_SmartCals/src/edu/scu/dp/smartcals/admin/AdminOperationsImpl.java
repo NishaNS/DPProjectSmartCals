5560fa022b55c52c12f793489fd21997c10ece39
aparna g <aparna.ganesh1542@gmail.com>
2015-08-18 15:13:06 -0700

    Code change Observer pattern in progress

--- DPProject_SmartCals/src/edu/scu/dp/smartcals/admin/AdminOperationsImpl.java
+++ DPProject_SmartCals/src/edu/scu/dp/smartcals/admin/AdminOperationsImpl.java
@@ -7,25 +7,30 @@ import edu.scu.dp.smartcals.vm.Product;
 
 /**
  * @author Aparna Ganesh
- *
+ * Admin operations implementations
  */
-public class AdminOperationsImpl implements AdminOperations {
+public class AdminOperationsImpl implements AdminOperations,VMUpdateListener {
 
-	/* (non-Javadoc)
-	 * @see edu.scu.dp.smartcals.admin.AdminOperations#reStockProduct(long, long, int)
-	 */
+	
 	@Override
 	public void reStockProduct(long vmId, long productId, int quantity) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.scu.dp.smartcals.admin.AdminOperations#addNewProduct(edu.scu.dp.smartcals.vm.Product, long)
-	 */
+	
 	@Override
 	public void addNewProduct(Product product, long vmId) {
 		// TODO Auto-generated method stub
+		
+	}
+
+
+	
+	@Override
+	public void updateOutOfStock(long vmId, long productId) {
+		
+		System.out.println("The product " +productId +" for the VM "+vmId + " is out of Stock");
 		
 	}
 
