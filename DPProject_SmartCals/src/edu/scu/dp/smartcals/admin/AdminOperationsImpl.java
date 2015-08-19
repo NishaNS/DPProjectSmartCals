3632232fb4c06fd64fff65f90991cf1674c05600
aparna g <aparna.ganesh1542@gmail.com>
2015-08-19 13:55:16 -0700

    code change for observer pattern

--- DPProject_SmartCals/src/edu/scu/dp/smartcals/admin/AdminOperationsImpl.java
+++ DPProject_SmartCals/src/edu/scu/dp/smartcals/admin/AdminOperationsImpl.java
@@ -3,35 +3,63 @@
  */
 package edu.scu.dp.smartcals.admin;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.scu.dp.smartcals.constants.Constants;
 import edu.scu.dp.smartcals.vm.Product;
 
 /**
- * @author Aparna Ganesh
- * Admin operations implementations
+ * @author Aparna Ganesh Admin operations implementations
  */
-public class AdminOperationsImpl implements AdminOperations,VMUpdateListener {
+public class AdminOperationsImpl implements AdminOperations, VMUpdateListener {
 
-	
+	/**
+	 * Code change-Aparna - 8/18 Added Alert to notify Monitoring Station View
+	 */
+	private List<AlertListener> alertListeners;
+
+	public AdminOperationsImpl() {
+		alertListeners = new ArrayList<>();
+	}
+
+	public void addAlertListeners(AlertListener alertListener) {
+
+		alertListeners.add(alertListener);
+
+	}
+
+	public void sendOutOfStockAlert(long vmId, long productId) {
+
+		Alert alert = new OutOfStockAlert();
+
+		alert.addProperty(Constants.PRODUCT_ID_KEY, productId + "");
+		alert.addProperty(Constants.VM_ID_KEY, vmId + "");
+
+		for (AlertListener alertListener : alertListeners) {
+			alertListener.update(alert);
+		}
+	}
+
+	// -------------code change-Aparna 8/18
+
 	@Override
 	public void reStockProduct(long vmId, long productId, int quantity) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
 	@Override
 	public void addNewProduct(Product product, long vmId) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-
-	
 	@Override
 	public void updateOutOfStock(long vmId, long productId) {
-		
-		System.out.println("The product " +productId +" for the VM "+vmId + " is out of Stock");
-		
+		// notify MonitoringStationView
+		 System.out.println("The product " +productId +" for the VM "+vmId + " is out of Stock");
+		 sendOutOfStockAlert(vmId, productId);
 	}
 
 }
