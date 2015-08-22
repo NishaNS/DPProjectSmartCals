d6fc2030da7819ff2277d67b753273e88d0e9f3b
aparna g <aparna.ganesh1542@gmail.com>
2015-08-22 11:06:08 -0700

    added Admin Exception

--- DPProject_SmartCals/src/edu/scu/dp/smartcals/admin/AdminOperations.java
+++ DPProject_SmartCals/src/edu/scu/dp/smartcals/admin/AdminOperations.java
@@ -6,6 +6,7 @@ package edu.scu.dp.smartcals.admin;
 import java.sql.SQLException;
 import java.util.List;
 
+import edu.scu.dp.smartcals.exception.AdminOperationsException;
 import edu.scu.dp.smartcals.model.ProductModel;
 import edu.scu.dp.smartcals.vm.Product;
 import edu.scu.dp.smartcals.vm.VendingMachine;
@@ -20,5 +21,5 @@ public interface AdminOperations {
 	
 	public void addNewProduct(Product product,long vmId);
 	
-	public List<Product> getBestSellingProduct(long vmId) throws SQLException;
+	public List<Product> getBestSellingProduct(long vmId) throws AdminOperationsException;
 }
